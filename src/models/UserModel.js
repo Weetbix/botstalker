62971e36df3a340257c3615fd355df474e22d015
John Hannagan <weetbix_@hotmail.com>
2016-05-16 18:44:27 +0200

    Add comment to user model

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,5 +1,9 @@
 import Backbone from 'backbone';
 
+// Single user from the slack web API. 
+// To fetch, requires:
+// id    - set to the user ID to request
+// token - set to the slack api key to use for fetching
 export default Backbone.Model.extend({
   url: function() {
     return `https://slack.com/api/users.info?token=${this.attributes.token}&user=${this.id}`;
